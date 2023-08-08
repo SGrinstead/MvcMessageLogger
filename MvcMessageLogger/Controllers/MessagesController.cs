40ed3a1654ec388124fb3b04f04fac47d801a1e2
SGrinstead <seth.grinstead3299@gmail.com>
2023-08-08 09:50:18 -0700

    massages index

---
+++ MvcMessageLogger/Controllers/MessagesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcMessageLogger.DataAccess;
+
+namespace MvcMessageLogger.Controllers
+{
+	public class MessagesController : Controller
+	{
+		private readonly MvcMessageLoggerContext _context;
+
+		public MessagesController(MvcMessageLoggerContext context)
+		{
+			_context = context;
+		}
+
+		[Route("/users/{userId:int}/messages")]
+		public IActionResult Index(int userId)
+		{
+			var user = _context.Users
+				.Where(u => u.Id == userId)
+				.Include(u => u.Messages)
+				.FirstOrDefault();
+
+			return View(user);
+		}
+	}
+}
