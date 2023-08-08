fdc4b59b0bc380c5cc1406419d1266cd219e2d55
SGrinstead <seth.grinstead3299@gmail.com>
2023-08-08 10:06:22 -0700

    new/create action and view

--- MvcMessageLogger/Controllers/MessagesController.cs
+++ MvcMessageLogger/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MvcMessageLogger.DataAccess;
+using MvcMessageLogger.Models;
 
 namespace MvcMessageLogger.Controllers
 {
@@ -22,6 +23,32 @@ namespace MvcMessageLogger.Controllers
 				.FirstOrDefault();
 
 			return View(user);
+		}
+
+		[Route("/users/{userId:int}/messages/new")]
+		public IActionResult New(int userId)
+		{
+			var user = _context.Users
+				.Where(u => u.Id == userId)
+				.Include(u => u.Messages)
+				.FirstOrDefault();
+
+			return View(user);
+		}
+
+		[HttpPost]
+		[Route("/users/{userId:int}/messages")]
+		public IActionResult Create(Message message, int userId)
+		{
+			message.CreatedAt = DateTime.Now.ToUniversalTime();
+			var user = _context.Users
+				.Where(u => u.Id == userId)
+				.Include(u => u.Messages)
+				.FirstOrDefault();
+			user.Messages.Add(message);
+			_context.SaveChanges();
+
+			return Redirect($"/users/{user.Id}/messages");
 		}
 	}
 }
